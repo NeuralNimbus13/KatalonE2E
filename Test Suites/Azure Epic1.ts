720444786daa7a003960e6727ad22dd63ab641ed
Pravin Raj Paul <pravinrajpaul@gmail.com>
2024-12-09 10:52:37 +0530

    Azure Tests

---
+++ Test Suites/Azure Epic1.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Azure Epic1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>58ad79af-d05a-4378-b93a-d7ecf3e7dcc4</testSuiteGuid>
+   <testCaseLink>
+      <guid>e9f36cd1-1be1-4d91-891b-89994535e505</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Azure/Epic1/Requirement1/E1R1 Test1 Test2 Test3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8800d7f-5cc3-449b-9ba0-e3d75e0359ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Azure/Epic1/Requirement2/E1R2 Test1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
